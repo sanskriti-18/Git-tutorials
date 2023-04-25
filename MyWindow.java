80804fa2fbb4318ca2df6b371a2cc8cf81e965a4
sanskriti-18 <130923272+sanskriti-18@users.noreply.github.com>
2023-04-25 18:00:58 +0530

    Add files via upload

---
+++ MyWindow.java
@@ -0,0 +1,58 @@
+package com.practice;
+
+import javax.swing.*;
+import java.awt.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.text.SimpleDateFormat;
+import java.util.Date;
+
+
+public class MyWindow extends JFrame
+{
+    private JLabel heading;
+    private JLabel clockLabel;
+
+    private Font font = new Font("",Font.BOLD,35);
+
+    MyWindow()
+    {
+        super.setTitle("My Clock");
+        super.setSize(400,400);
+        super.setLocation(300, 250);
+        this.createGUI();
+        this.startClock();
+        super.setVisible(true);
+    }
+    public void  createGUI()
+    {
+heading=new JLabel("MY CLOCK");
+clockLabel = new JLabel("clock");
+heading.setFont(font);
+clockLabel.setFont(font);
+
+this.setLayout(new GridLayout(2,1));
+this.add(heading);
+this.add(clockLabel);
+
+    }
+    public void startClock()
+    {
+
+     Timer timer=new Timer(1000, new ActionListener() {
+         @Override
+         public void actionPerformed(ActionEvent e) {
+            // String dateTime=new Date().toString();
+          //   String dateTime=new Date().toLocaleString();
+             Date d=new Date();
+             SimpleDateFormat sfd=new SimpleDateFormat("hh : mm: ss a");
+             String dateTime=sfd.format(d);
+             clockLabel.setText(dateTime);
+
+         }
+     });
+     timer.start();
+
+
+    }
+}
